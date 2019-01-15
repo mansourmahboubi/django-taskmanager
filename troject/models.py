ecd7100607ed4a533adf296108b57d6ca4b67fd8
M Mansour Mahboubi <mm.mahboubi@ut.ac.ir>
2019-01-15 17:06:24 +0330

    group model added

--- troject/models.py
+++ troject/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 
+class Group(models.model):
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Task(models.model):
+    group = models.ForeignKey(Group)
     status = models.BooleanField(default=True)
     title = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.status
