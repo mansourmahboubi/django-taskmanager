703e4c6f6320d5c721d2935e8cac65b3afc1aa95
M Mansour Mahboubi <mm.mahboubi@ut.ac.ir>
2019-01-15 16:59:17 +0330

    task model updated

--- troject/models.py
+++ troject/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.model):
+    status = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status
