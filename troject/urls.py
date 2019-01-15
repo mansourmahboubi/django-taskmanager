9fd095271dbfce777f9a4ed6bce114d60ffb96fe
M Mansour Mahboubi <mm.mahboubi@ut.ac.ir>
2019-01-15 20:42:04 +0330

    create & delete generic views added

--- troject/urls.py
+++ troject/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import TaskListView
+from .views import TaskListView, task_list, Taskcreate, TaskDelete
 
 
 urlpatterns = [
-    url(r'^$', TaskListView.as_view(), name='task-list'),
+    url(r'^test', TaskListView.as_view(), name='task-list'),
+    url(r'^create/(?P<pk>\d+)$', Taskcreate.as_view(), name='list_update'),
+    url(r'^delete/(?P<pk>\d+)$', TaskDelete.as_view(), name='list_update'),
+    url(r'^$', task_list, name='tasklist'),
 ]
--- troject/views.py
+++ troject/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Task
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Task, Group
 
 
 # https://docs.djangoproject.com/en/1.11/ref/class-based-views/generic-display/
 class TaskListView(ListView):
-
     model = Task
+
+
+# https://docs.djangoproject.com/en/1.11/ref/class-based-views/generic-editing/
+class Taskcreate(CreateView):
+    model = Task
+    fields = ['group', 'title']
+
+
+class TaskDelete(DeleteView):
+    model = Task
+
+
+def task_list(request):
+    context = {}
+    Tasks = Task.objects.all()
+    #  predefined groups = [ todo, doing ,done]
+    context['todo'] = Tasks.filter(group__title='todo')[:5]
+    context['doing'] = Tasks.filter(group__title='doing')[:5]
+    context['done'] = Tasks.filter(group__title='doing')[:5]
+    return render(request, 'troject/task.html', context)
