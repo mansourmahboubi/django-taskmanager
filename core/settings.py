cd2693b5df3046cf003e2000c941c41d98efe58f
M Mansour Mahboubi <mm.mahboubi@ut.ac.ir>
2019-01-16 12:24:42 +0330

    setting update

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PERSISTENT_STORAGE = '/home/m_mans0ur/Code/callofduty/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(PERSISTENT_STORAGE, "statics")
+STATIC_ROOT = os.path.join(BASE_DIR, "statics")
