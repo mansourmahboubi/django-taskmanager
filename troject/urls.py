80b034004076a6898d546eaac0e678e5ad9a8bb2
M Mansour Mahboubi <mm.mahboubi@ut.ac.ir>
2019-01-22 01:27:34 +0330

    views integrated to multiuser

--- troject/urls.py
+++ troject/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from .views import TaskListView, task_list, Taskcreate, TaskDelete, change_status
+from .views import task_list, Taskcreate, TaskDelete, change_status
 
 urlpatterns = [
-    url(r'^test', TaskListView.as_view(), name='task-list'),
     url(r'^$', task_list, name='tasklist'),
     url(r'^edit/create/$', Taskcreate.as_view(), name='list_update'),
     url(r'^edit/delete/(?P<pk>\d+)', TaskDelete.as_view(), name='list_update'),
--- troject/views.py
+++ troject/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView
-from .models import Task
-
-
-# https://docs.djangoproject.com/en/1.11/ref/class-based-views/generic-display/
-class TaskListView(ListView):
-    model = Task
+from .models import Task, Group
 
 
 # https://docs.djangoproject.com/en/1.11/ref/class-based-views/generic-editing/
@@ -14,31 +8,38 @@ class Taskcreate(CreateView):
     model = Task
     fields = ['group', 'title']
 
+    def post(self, request, **kwargs):
+        request.POST = request.POST.copy()
+        request.POST['user'] = self.request.user.id
+        return super(Taskcreate, self).post(request, **kwargs)
+
 
 class TaskDelete(DeleteView):
-    # limiting the query set to avoid unprivileged deleting
-    queryset = Task.objects.all().filter(status=False)
     model = Task
     success_url = '/'
 
+    # limiting the query set to avoid unprivileged deleting
+    def get_queryset(self):
+        queryset = super(TaskDelete, self).get_queryset()
+        return queryset.filter(status=False, user__id=self.request.user.id)
 # the main function
 # it displays home page with task groups
 
 
 def task_list(request, pk=None):
     context = {}
-    Tasks = Task.objects.all().filter(user__id=request.user.id).order_by('-updated')
+    tasks = Task.objects.all().filter(user__id=request.user.id).order_by('-updated')
     #  predefined groups = [ home, work ,entertainment]
-    context['home'] = Tasks.filter(group__title='home')
-    context['work'] = Tasks.filter(group__title='work')
-    context['entertainment'] = Tasks.filter(group__title='entertainment')
+    context['home'] = tasks.filter(group__title='home')
+    context['work'] = tasks.filter(group__title='work')
+    context['entertainment'] = tasks.filter(group__title='entertainment')
     return render(request, 'troject/task.html', context)
 
 # call it through a button and it changes task status
 
 
 def change_status(request, pk):
-    task = get_object_or_404(Task, id=pk)
+    task = get_object_or_404(Task, id=pk, user__id=request.user.id)
     task.status = not task.status
     task.save()
     return redirect('/')
